0000b655c95002d4b11ba7cfe0dea45389615517
José Enrique de la Torre Leyva <kikedlt27@gmail.com>
2023-11-17 11:23:49 -0600

    Update Appjsx
    je je je

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,6 @@ import Home from './components/Home'
 import Nosotros from './components/Nosotros'
 
 function App() {
-  
-  const productos =[
-    {id: 1, titlo: "Producto A", descripcion: "Descripcion del producto", precio: 1000, Categoria: "A"},
-    {id: 2, titlo: "Producto B", descripcion: "Descripcion del producto", precio: 100, Categoria: "B"},
-    {id: 3, titlo: "Producto C", descripcion: "Descripcion del producto", precio: 2000, Categoria: "C"},
-    {id: 4, titlo: "Producto D", descripcion: "Descripcion del producto", precio: 1500, Categoria: "D"},
-  ]
-
-  const promesaProductos = new Promise((resolve, reject)=>{
-    if(productos.length > 0){
-      setTimeout(()=>{
-        resolve(productos)
-      }, 5000)  
-      }else{
-        reject("No se resolvo la promesa")
-      }    
-  })
-
-  promesaProductos
-    .then((resultado)=>{
-      console.log(resultado)
-    })
-  
-    .catch((error)=>{
-      console.log(error)
-    })
-  
-
 
   return (
     <Router>
@@ -41,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Nosotros" element={<Nosotros />} />
-        <Route path="/Contenedor" element={<ItemListContainers greeting={"Hola mundo"} />} />
+        <Route path="/Contenedor" element={<ItemListContainers />} />
       </Routes>
     </Router>
   );
